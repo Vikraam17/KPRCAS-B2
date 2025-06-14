6cff35f83c6786e07439b9bbb1961b6313069bfd
Viky <vikraam17@gmail.com>
2025-06-14 15:49:02 +0530

    js added

---
+++ JavaScript/script.js
@@ -0,0 +1,88 @@
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(10,20)
+
+// // Arrow Function
+// var sum = ()=>{
+//     var a=10;
+//     var b=20;
+//     console.log(a+b)
+// }
+// sum()
+
+//Spread Operator
+let arr = [10,20,30,40];
+let arr2 =[...arr,50,60,70]
+console.log(arr)
+console.log(arr2)
+
+//Destructuring operator
+var marks =[90,95,96,100,99];
+var [m1,m2,m3,m4,m5] = marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+
+
+
+
+
+
+
+
+// var a="10";
+// var b=10;
+
+// console.log(a==b); //true
+// console.log(a===b) //false
+// console.log(a!=b); //false
+// console.log(a!==b); //true
+
+// var num=2;
+// var result = num%2===0? "Even":"odd"
+// console.log(result)
+
+// var mark=34;
+// var result = (mark>=80)? "O":(mark>=60? "A+":(mark>=35?"P":"F"))
+// console.log(result)
+
+// if(mark>=80 && mark<=100){
+//     console.log("O")
+// }
+// else if(mark>=60 && mark<80){
+//     console.log("A")
+// }
+// else if(mark>=35 && mark<60){
+//     console.log("P")
+// }
+// else{
+//     console.log("F")
+// }
+// var i=1
+// for(;i<=10;i++){
+//     console.log(i)
+// }
+// var i=1;
+// while(i<=10){
+//     console.log(`The value of i is ${i}`)
+//     i++;
+// }
+// do{
+//     console.log(i)
+// }while(i<=10);
+
+
+// console.log(a*b)
+// console.log(typeof a);
+// console.log(typeof b);
+// a=true
+// console.log(typeof a)
+// var c;
+// console.log(typeof c)
+// var c = null;
+// console.log(c)
+
